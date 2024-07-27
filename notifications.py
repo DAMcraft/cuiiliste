de112fce1bccb1d0f866048564d34a96e91cd768
DAM <43420467+DAMcraft@users.noreply.github.com>
2024-07-27 13:06:56 +0200

    notification system reworked

--- notifications.py
+++ notifications.py
@@ -2,17 +2,76 @@ import os
 import requests
 
 
-def send_notif(message: str, should_ping: bool = True):
-    # Send a message to a discord webhook (for now at least)
-    # I don't want to use another requirement for this, so I'll just use urllib
+def send_webhook(data: dict):
     webhook_url = os.getenv("WEBHOOK_URL")
     if not webhook_url:
         return
-    message = "@everyone " + message if should_ping else message
-    data = {
-        "content": message,
-        "allowed_mentions": {
-            "parse": ["everyone"]
-        }
-    }
     requests.post(webhook_url, json=data)
+
+
+def blocking_instance_removed(domain: str, isp: str):
+    send_webhook({
+        "content": f"<@&{os.getenv('CUII_NOTIF_ROLE_ID')}>",
+        "embeds": [
+            {
+                "title": f"Domain von {isp} entsperrt",
+                "description": f"Die Domain {domain} wurde von {isp} entsperrt.",
+                "color": 0x57F287
+            }
+        ],
+        "allowed_mentions": {
+            "roles": [os.getenv('CUII_NOTIF_ROLE_ID')]
+        }
+    })
+
+
+def blocking_instance_added(domain: str, isp: str):
+    send_webhook({
+        "content": f"<@&{os.getenv('CUII_NOTIF_ROLE_ID')}>",
+        "embeds": [
+            {
+                "title": "Domain gesperrt",
+                "description": f"Die Domain {domain} wurde von {isp} gesperrt.",
+                "color": 0xED4245
+            }
+        ],
+    })
+
+
+def domain_unblocked(domain: str):
+    send_webhook({
+        "content": f"<@&{os.getenv('CUII_NOTIF_ROLE_ID')}>",
+        "embeds": [
+            {
+                "title": "Domain entsperrt",
+                "description": f"Die Domain {domain} wurde komplett entsperrt.",
+                "color": 0x57F287
+            }
+        ],
+    })
+
+
+def domain_blocked(domain: str):
+    send_webhook({
+        "content": f"<@&{os.getenv('CUII_NOTIF_ROLE_ID')}>",
+        "embeds": [
+            {
+                "title": "Domain gesperrt",
+                "description": f"Die Domain {domain} wurde gesperrt.",
+                "color": 0xED4245
+            }
+        ],
+    })
+
+
+def error(message: str):
+    send_webhook({
+        "content": f"",
+        "embeds": [
+            {
+                "title": "Fehler",
+                "description": message,
+                "color": 0xED4245
+            }
+        ]
+    })
